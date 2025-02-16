918774fc134cf861f5ad0c97a72a15c2570ac566
Jeyreet <jeyreet@yandex.ru>
2025-02-17 01:55:53 +0700

    Initial commit: added layout

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+document.querySelector('.header__burger').addEventListener('click', () => {
+    document.querySelector('.header__menu-outer').classList.toggle('visible');
+})
+
+document.querySelector('.header__menu-outer').addEventListener('click', () => {
+    document.querySelector('.header__menu-outer').classList.toggle('visible');
+})
